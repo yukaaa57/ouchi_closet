a8493b060e20e7d015d8e0c5ce211b10babddc47
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-02-16 16:05:27 +0900

    アカウント画面エラー解消（views.py修正）

--- my_app/accounts/views.py
+++ my_app/accounts/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import get_user_model
 from .forms import SignUpForm
 
+User = get_user_model()
+
 class SignUpView(CreateView):
-    from_class = SignUpForm
+    model = User
+    form_class = SignUpForm
     template_name = "registration/signup.html"
-    succes_url = reverse_lazy("login")
+    success_url = reverse_lazy("login")
     
 # Create your views here.
