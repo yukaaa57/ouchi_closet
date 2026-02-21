17b86aedfe55803f39d5e725b0f0f5043a0d8d2f
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-02-22 02:38:41 +0900

    class SignUpView内にform_valid追加（招待なしでアカウント登録した際の処理）

--- my_app/accounts/views.py
+++ my_app/accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model,login
 from .forms import SignUpForm
+from .models import Family
+
 
 User = get_user_model()
 
@@ -10,7 +12,16 @@ class SignUpView(CreateView):
     model = User
     form_class = SignUpForm
     template_name = "registration/signup.html"
-    success_url = reverse_lazy("login")
+    
+    def form_valid(self, form):
+        user = form.save()
+        family = Family.objects.create()
+        user.family = family
+        user.save
+        login(self.request, user)
+        return redirect("/")
+    
+    success_url = reverse_lazy("/")
     
 
         
