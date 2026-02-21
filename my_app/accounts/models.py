a4372a309acb1a1fea3703cf1ea7d086ce795337
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-02-22 01:11:45 +0900

    ファミリークラス作成

--- my_app/accounts/models.py
+++ my_app/accounts/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, BaseUserManager)
+import uuid
+from django.conf import settings
+from django.utils import timezone
+from datetime import timedelta
 
 class UserManager(BaseUserManager):
     
@@ -8,23 +12,16 @@ class UserManager(BaseUserManager):
             raise ValueError("メールは必須です")
         
         email = self.normalize_email(email)
-        
         user = self.model(email=email)
-        
         user.set_password(password)
-        
         user.save(using=self._db)
-        
         return user
+    
 def create_superuser(self, email, password=None):
-    
     user = self.create_user(email, password)
-    
     user.is_staff = True
     user.is_superuser = True
-    
     user.save(using=self._db)
-    
     return user
 
 SIZE_CHOICES = [
@@ -47,6 +44,12 @@ SIZE_CHOICES = [
     ("その他","その他"),   
 ]
 
+class Family(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Family {self.id}"
+
 class User(AbstractBaseUser, PermissionsMixin):
     profile_image = models.ImageField(
         verbose_name="プロフィール画像",
@@ -54,12 +57,10 @@ class User(AbstractBaseUser, PermissionsMixin):
         null=True,
         blank=True,
     )
-    
     nickname = models.CharField(
         verbose_name="ニックネーム",
         max_length=100,
     )
-    
     size = models.CharField(
         verbose_name="サイズ",
         max_length=10,
@@ -67,13 +68,11 @@ class User(AbstractBaseUser, PermissionsMixin):
         blank=True,
         default="",
     )
-    
     email = models.EmailField(
         verbose_name='Eメールアドレス',
         max_length=255,
         unique=True,
     )
-    
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
