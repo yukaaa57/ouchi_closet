a43243a46b75b095aa09870821e9f3131979b295
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-02-23 01:28:41 +0900

    accounts/urls.pyにpath追記

--- my_app/accounts/urls.py
+++ my_app/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SignUpView
+from . import views
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("invite/", views.invite_view, name="invite"),
 ]
