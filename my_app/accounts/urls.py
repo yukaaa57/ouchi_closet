64424c53270591bb6ae90ce6fb4b91760ddf9988
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-02-28 23:37:58 +0900

    urls.pyにパスワード変更画面のpath追加

--- my_app/accounts/urls.py
+++ my_app/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("invite/", views.invite_view, name="invite"),
     path("me/", views.me_view, name="me"),
     path("me/edit/", views.ProfileUpdateView.as_view(), name="account_edit"),
+    path("password_change/", views.CustomPasswordChangeView.as_view(), name="password_change"),
 ]
