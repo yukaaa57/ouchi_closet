1c6432a7642f8be52147d4cbe86cfff228378897
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-02-28 18:30:11 +0900

    forms.pyにclass ProfileUpdateForm（アカウント編集）作成

--- my_app/accounts/forms.py
+++ my_app/accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
 User = get_user_model()
 
@@ -8,4 +9,9 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("profile_image", "nickname", "size", "email")
-    
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("profile_image", "nickname", "size", "email")
+        
