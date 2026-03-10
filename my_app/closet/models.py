4db098ad0809c8a9c5f464ec49b8d5bd33f36427
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-10 22:44:01 +0900

    closet/models.pyにSeasonクラス作成

--- my_app/closet/models.py
+++ my_app/closet/models.py
@@ -11,4 +11,9 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+class Season(models.Model):
+    name = models.CharField(max_length=20, unique=True)
     
+    def __str__(self):
+        return self.name 
