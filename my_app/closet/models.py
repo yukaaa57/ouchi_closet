e2151bb685d0cd4ea28b3478fd8fc19f7da90e59
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-10 22:41:34 +0900

    closet/models.pyにCategoryクラス作成

--- my_app/closet/models.py
+++ my_app/closet/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from accounts.models import Child, SIZE_CHOICES
 
-# Create your models here.
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
