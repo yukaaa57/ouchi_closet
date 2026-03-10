3cf06f3ed78dd7cf9387f662dba13fda8550b965
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-10 23:17:00 +0900

    closet/models.pyにClothingItemクラス作成

--- my_app/closet/models.py
+++ my_app/closet/models.py
@@ -17,3 +17,77 @@ class Season(models.Model):
     
     def __str__(self):
         return self.name 
+
+class ClothingItem(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="clothes",
+    )
+    
+    child = models.ForeignKey(
+        Child,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="clothes",
+    )
+    
+    clothing_image = models.ImageField(upload_to="clothing_photos/")
+    
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="clothes",
+    )
+    
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES)
+    
+    COLOR_CHOICES =(
+        ("white", "白"),
+        ("black", "黒"),
+        ("navy", "紺"),
+        ("brown", "茶"),
+        ("beige", "ベージュ"),
+        ("gray", "グレー"),
+        ("khaki", "カーキ"),
+        ("green", "緑"),
+        ("blue", "青"),
+        ("red", "赤"),
+        ("pink", "ピンク"),
+        ("purple", "紫"),
+        ("yellow", "黄"),
+        ("orange", "オレンジ"),
+        ("other", "その他"),
+    )
+    color = models.CharField(max_length=20, choices=COLOR_CHOICES)
+    
+    seasons = models.ManyToManyField(Season, related_name="clothes")
+    
+    WEAR_STATUS_CHOICES =(
+        ("unused", "未使用"),
+        ("justsize", "ジャストサイズ"),
+        ("large", "大きめ"),
+        ("small", "小さめ"),
+        ("rarely", "あまり着ていない"),
+        ("sizeout", "サイズアウト"),
+    )
+    wear_status =models.CharField(max_length=20, choices=WEAR_STATUS_CHOICES, blank=True)
+    
+    note = models.TextField(blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        if self.user:
+            owner_name = self.user.nickname
+        elif self.child:
+            owner_name = self.child.nickname
+        else:
+            owner_name = "未設定"
+        
+        return f"{owner_name} / {self.category.name}"
+    
