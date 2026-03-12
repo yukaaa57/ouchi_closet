2b3cdaa603e48629cb73a6672f70b6ea038fb759
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-12 16:59:55 +0900

    closet/admin.pyへcategory,season,clothingitemを追加

--- my_app/closet/admin.py
+++ my_app/closet/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Season, ClothingItem
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at")
+
+@admin.register(Season)
+class SeasinAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    
+@admin.register(ClothingItem)
+class ClothingItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "child", "category", "size", "color", "seasons", "wear_status", "created_at")
+    
+    
+
