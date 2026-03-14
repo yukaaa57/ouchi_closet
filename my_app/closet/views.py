f45ee6ee335370b7abd4b0acbbc563c4c275525b
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-14 16:27:43 +0900

    closet/views.pyにuser_closet,child_closet追加

--- my_app/closet/views.py
+++ my_app/closet/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from accounts.models import User, Child
+from .models import ClothingItem, Category
 
-# Create your views here.
+@login_required
+def user_closet(request, pk):
+    user = get_object_or_404(
+        User,
+        pk=pk,
+        family=request.user.family
+    )
+    
+    categories = Category.objects.all()
+    
+    context = {
+        "owner": user,
+        "owner_type": "user",
+        "categories": categories,
+    }
+    
+    return render(request, "closet/user_closet.html", context)
+    
+@login_required
+def child_closet(request, pk):
+    child = get_object_or_404(
+        Child,
+        pk=pk,
+        family=request.user.family
+    )
+    
+    categories = Category.objects.all()
+    
+    context = {
+        "owner": child,
+        "owner_type": "childr",
+        "categories": categories,
+    }
+    
+    return render(request, "closet/child_closet.html", context)
+
