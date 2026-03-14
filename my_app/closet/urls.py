cae31bd57d9fbc92cb515e07370c25db7331af90
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-15 00:04:51 +0900

    closet/urls.pyにclothing_list追加

--- my_app/closet/urls.py
+++ my_app/closet/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("user/<int:pk>/", views.user_closet, name="user_closet"),
     path("child/<int:pk>/", views.child_closet, name="child_closet"),
+    path("<str:owner_type>/<int:owner_id>/category/<int:category_id>", views.clothing_list, name="clothing_list")
 ]
