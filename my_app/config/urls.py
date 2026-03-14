6f544b7ee291734f56422cc5f4f959f697da86a4
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-14 16:57:24 +0900

    config/urls.pyにcloset追加

--- my_app/config/urls.py
+++ my_app/config/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     ),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("closet/", include("closet.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
