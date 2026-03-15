05ff1b2bc98b812ac2c32c6a209d1dc028d5d995
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-15 18:04:04 +0900

    closet/urls.py

--- my_app/closet/urls.py
+++ my_app/closet/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path("user/<int:pk>/", views.user_closet, name="user_closet"),
     path("child/<int:pk>/", views.child_closet, name="child_closet"),
-    path("<str:owner_type>/<int:owner_id>/all/", views.clothing_list_all, name="clothing_list_all"),
-    path("<str:owner_type>/<int:owner_id>/category/<int:category_id>/", views.clothing_list, name="clothing_list")
+    path("<str:owner_type>/<int:owner_id>/category/<str:category_id>/", views.clothing_list, name="clothing_list")
 ]
