49f9e2fa7559debed6762a8485c9d582cadb2002
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-15 23:22:00 +0900

    closet/urls.py,clothing_listの<str:category_id>を<str:category>へ変更

--- my_app/closet/urls.py
+++ my_app/closet/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("user/<int:pk>/", views.user_closet, name="user_closet"),
     path("child/<int:pk>/", views.child_closet, name="child_closet"),
-    path("<str:owner_type>/<int:owner_id>/<str:category_id>/", views.clothing_list, name="clothing_list")
+    path("<str:owner_type>/<int:owner_id>/<str:category>/", views.clothing_list, name="clothing_list")
 ]
