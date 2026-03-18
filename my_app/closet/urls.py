856f8e0c2ae847c15ef429ad1ed3af4483d4d9a8
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-19 00:44:03 +0900

    closet/urls.pyのpathの順番修正

--- my_app/closet/urls.py
+++ my_app/closet/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("user/<int:pk>/", views.user_closet, name="user_closet"),
     path("child/<int:pk>/", views.child_closet, name="child_closet"),
-    path("<str:owner_type>/<int:owner_id>/<str:category>/", views.clothing_list, name="clothing_list"),
     path("<str:owner_type>/<int:owner_id>/create/", views.clothing_create, name="clothing_create"),
     path("item/<int:pk>/edit/", views.clothing_update, name="clothing_update"),
+    path("<str:owner_type>/<int:owner_id>/<str:category>/", views.clothing_list, name="clothing_list"),
 ]
