273c4fb0293e8adfbb05cf2f8f3074b519f651e9
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-22 23:35:52 +0900

    loset/urls.pyにcategory_create（カテゴリ追加）追加

--- my_app/closet/urls.py
+++ my_app/closet/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("<str:owner_type>/<int:owner_id>/create/", views.clothing_create, name="clothing_create"),
     path("<str:owner_type>/<str:owner_id>/search", views.clothing_search, name="clothing_search"),
     path("<str:owner_type>/<str:owner_id>/search/results", views.clothing_search_results, name="clothing_search_results"),
+    path("category/create", views.category_create, name="category_create"),
     path("item/<int:pk>", views.clothing_detail, name="clothing_detail"),
     path("item/<int:pk>/edit/", views.clothing_update, name="clothing_update"),
     path("item/<int:pk>/delete", views.clothing_delete, name="clothing_delete"),
