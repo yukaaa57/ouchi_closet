a8595b8580939b56025e851adabc5e2d557b088c
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-23 21:44:24 +0900

    home.html修正（画像なしでも表示されるようif分追記）

--- my_app/cordinate/admin.py
+++ my_app/cordinate/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Outfit, OutfitClothingItem, OutfitImage
 
-# Register your models here.
+class OutfitClothingItemInline(admin.TabularInline):
+    model = OutfitClothingItem
+    extra = 1
+    
+class OutfitImageInline(admin.TabularInline):
+    model = OutfitImage
+    extra = 1
+    
+@admin.register(Outfit)
+class OutfirAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "child", "outfit_type", "is_favorite", "created_at")
+    inlines = [OutfitClothingItemInline, OutfitImageInline]    
+    
+@admin.register(OutfitClothingItem)
+class OutfitlothingItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "outfit", "clothing_item")
+    
+@admin.register(OutfitImage)
+class OutfitImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "outfit", "outfit_image")
