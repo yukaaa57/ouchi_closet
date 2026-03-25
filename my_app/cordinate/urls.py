244f9a32853bf72bc1e90f767d1ad24e8e9318ff
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-25 21:29:15 +0900

    cordinate/urls.pyにコーデ詳細追加

--- my_app/cordinate/urls.py
+++ my_app/cordinate/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("user/<int:owner_id>/", views.outfit_list, {"owner_type": "user"}, name="user_outfit_list"),
     path("child/<int:owner_id>/", views.outfit_list, {"owner_type": "child"}, name="child_outfit_list"),
+    path("user/<int:owner_id>/<int:pk>/", views.outfit_detail, {"owner_type": "user"}, name="user_outfit_detail"),
+    path("child/<int:owner_id>/<int:pk>/", views.outfit_detail, {"owner_type": "child"}, name="child_outfit_detail"),
     path("item/<int:pk>/favorite/", views.outfit_toggle_favorite, name="outfit_toggle_favorite"),
 ]
