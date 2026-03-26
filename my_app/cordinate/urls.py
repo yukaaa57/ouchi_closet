7a7a731e478066a31c5c6ee3f4dbb1064c743a44
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-26 23:40:05 +0900

    cordinate/urls.py/outfit_delete追加

--- my_app/cordinate/urls.py
+++ my_app/cordinate/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("child/<int:owner_id>/", views.outfit_list, {"owner_type": "child"}, name="child_outfit_list"),
     path("item/<int:pk>/", views.outfit_detail, name="outfit_detail"),
     path("item/<int:pk>/edit/", views.outfit_update, name="outfit_update"),
+    path("item/<int:pk>/delete/", views.outfit_delete, name="outfit_delete"),
     path("item/<int:pk>/favorite/", views.outfit_toggle_favorite, name="outfit_toggle_favorite"),
 ]
