df127a12a143126fe8c093549cf20ffb60634864
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-03-27 16:59:06 +0900

    cordinate/urls.py/favorite_outfit_list(お気に入りコーデ一覧）追加

--- my_app/cordinate/urls.py
+++ my_app/cordinate/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("user/<int:owner_id>/", views.outfit_list, {"owner_type": "user"}, name="user_outfit_list"),
     path("child/<int:owner_id>/", views.outfit_list, {"owner_type": "child"}, name="child_outfit_list"),
+    path("user/<int:owner_id>/favorite/", views.favorite_outfit_list, {"owner_type": "user"}, name="user_favorite_outfit_list"),
+    path("child/<int:owner_id>/favorite/", views.favorite_outfit_list, {"owner_type": "child"}, name="child_favorite_outfit_list"),
     path("item/<int:pk>/", views.outfit_detail, name="outfit_detail"),
     path("item/<int:pk>/edit/", views.outfit_update, name="outfit_update"),
     path("item/<int:pk>/delete/", views.outfit_delete, name="outfit_delete"),
