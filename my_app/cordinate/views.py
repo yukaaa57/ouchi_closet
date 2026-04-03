d40a25d60f20ae167a3b5be30d04d835e38d7a7d
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-04-04 00:15:32 +0900

    cordinate/views.py/outfit_update,search内categotries=の記載を仮で変更

--- my_app/cordinate/views.py
+++ my_app/cordinate/views.py
@@ -159,7 +159,7 @@ def outfit_update(request, pk):
                 id__in=outfit.outfit_clothing_items.values_list("clothing_item_id", flat=True)
             )
         
-        categories = Category.objects.filter(family=request.user.family)
+        categories = Category.objects.all()
         color_choices = ClothingItem.COLOR_CHOICES
         
     else:
@@ -207,7 +207,7 @@ def clothing_item_search(request, pk):
     current_item_ids = outfit.outfit_clothing_items.values_list("clothing_item_id", flat=True)
     clothing_items = clothing_items.exclude(id__in=current_item_ids)
     
-    categories = Category.objects.filter(family=request.user.family)
+    categories = Category.objects.all()
     color_choices = ClothingItem.COLOR_CHOICES
     
     context = {
