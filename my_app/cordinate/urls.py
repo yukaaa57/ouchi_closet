27ed4d140624d84c4b6d8ddd6832743cc3be0ef6
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-04-14 23:21:37 +0900

    cordinate/urls.py/nursery_item_create（保育園必需品）追加

--- my_app/cordinate/urls.py
+++ my_app/cordinate/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path("item/image/<int:pk>/delete", views.outfit_image_delete, name="outfit_image_delete"),
     path("item/<int:pk>/favorite/", views.outfit_toggle_favorite, name="outfit_toggle_favorite"),
     path("item/<int:pk>/clothing-search", views.clothing_item_search, name="clothing_item_search"),
+    path("nursery/<int:child_id>/create/", views.nursery_item_create, name="nursery_item_create"),
 ]
