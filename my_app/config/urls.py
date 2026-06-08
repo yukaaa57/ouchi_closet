ffd3a0b252bf8d86e87ce39ee4874cbaaee76b44
Macintosh <macintosh@makkintosshunoMacBook-Air.local>
2026-06-08 22:05:17 +0900

    パスリセットメール本文修正

--- my_app/config/urls.py
+++ my_app/config/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("accounts/password_reset/",
          auth_views.PasswordResetView.as_view(
              template_name="registration/password_reset_form.html",
-             email_template_name="registration/password_reset_email.html",
+             email_template_name="registration/password_reset_email.txt",
              subject_template_name="registration/password_reset_subject.txt",
          ),
          name="password_reset",
